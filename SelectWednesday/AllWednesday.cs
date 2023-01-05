f5e38126fb074f3f54b8b7c3cf75202436e053a5
Jia <vernalin1422@gmail.com>
2023-01-05 20:52:58 +0800

    加入專案檔案。

---
+++ SelectWednesday/AllWednesday.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectWednesday
+{
+    public class AllWednesday
+    {
+		//List<DateTime> dateTimes = new List<DateTime>(); 
+        public static string Wednesday()
+        {
+            int yearday = 365;
+            string result =string.Empty;
+
+            DateTime dtbegin = new DateTime(2023, 1, 1);
+            DateTime dtend = new DateTime(2023, 12, 31);
+            
+			
+            for(int i = 0; i <= yearday; i++)
+            {
+                if (dtbegin.DayOfWeek == (DayOfWeek)3) result += dtbegin.ToString("yyyy/MM/dd")+"\n";
+				if (dtbegin <= dtend) dtbegin = dtbegin.AddDays(1);
+
+			}
+
+			return result;
+		}
+    }
+}
---
+++ Wednesday/Program.cs
@@ -0,0 +1,17 @@
+﻿using SelectWednesday;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wednesday
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(AllWednesday.Wednesday());
+		}
+	}
+}
