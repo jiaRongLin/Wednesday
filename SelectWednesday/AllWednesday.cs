348a7b37f870c60c6484cf3aaae86c0b36eec51f
Jia <vernalin1422@gmail.com>
2023-01-06 13:17:14 +0800

    更新

--- SelectWednesday/AllWednesday.cs
+++ SelectWednesday/AllWednesday.cs
@@ -18,18 +18,30 @@ namespace SelectWednesday
 		/// <returns>回傳2023年所有星期三的日期</returns>
 		public static string Wednesday()
         {
-            int yearday = 365; //一年365天
+            int Day = 7; //一周7天
+			int week = 52;//一年52周
             string result =string.Empty;
 
             DateTime dtbegin = new DateTime(2023, 1, 1); //起始日
             DateTime dtend = new DateTime(2023, 12, 31); //結束日
             DayOfWeek dayOfWeek = (DayOfWeek)3; //星期三
-            
-			
-            for(int i = 0; i <= yearday; i++)
-            {
-                if (dtbegin.DayOfWeek == dayOfWeek) result += dtbegin.ToString("yyyy/MM/dd")+"\n";
-				if (dtbegin <= dtend) dtbegin = dtbegin.AddDays(1);
+
+
+			for (int i = 1; i <= Day; i++)
+			{
+				if (dtbegin.DayOfWeek == dayOfWeek)
+				{
+					result += dtbegin.ToString("yyyy/MM/dd") + "\n";
+					break;
+				}
+
+				dtbegin = dtbegin.AddDays(1);
+			}
+
+			for (int j = 1; j <= week; j++)
+			{
+				if (dtbegin < dtend) dtbegin = dtbegin.AddDays(7);
+				if( dtbegin < dtend) result += dtbegin.ToString("yyyy/MM/dd") + "\n";
 
 			}
 
